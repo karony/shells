95d4dd810fa36b515c7946fffa72043132f5bd19
karony <964651844@qq.com>
2017-06-27 15:26:06 +0800

    Delete backup_mysqldump.sh

--- backups/backup_mysqldump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -
-#Description: create full bakcups with mysqldump
-#Author:      karony
-#Data:        2017-06-27
-#
-[[ ! -d /data/backups ]] && mkdir -p /data/backups/ > /dev/null
-mysqldump --user=root --password=root --host=localhost --all-databases --flush-log --master-data=2 --single-transaction -E --trigger -R > /data/backups/mysql.$(date +%F)
