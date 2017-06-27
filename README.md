539f713e1172fdbcb4d684141b1ff579334c7798
root <node1@lirou.com>
2017-06-27 15:22:34 +0800

    mysqldump

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # shells
+all shells
---
+++ backups/mysqldump/full_backup_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -
+#Description: create full bakcups with mysqldump
+#Author:      karony
+#Data:        2017-06-27
+#
+
+backup_dir=/data/backups
+db_user=root
+db_passwd=root
+db_host=localhost
+
+[[ ! -d $backup_dir ]] && mkdir -p $backup_dir > /dev/null
+
+mysqldump --user=$db_user --password=$db_passwd --host=$db_host --all-databases --flush-log --master-data=2 --single-transaction -E --trigger -R > $backup_dir/mysql.$(date +%F)
+
+find $backup_dir -mtime +5 -exec rm -rf {} \;
