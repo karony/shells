0291d3486c5ef1c49915080f88740668c46d5588
karony <964651844@qq.com>
2018-01-31 11:20:23 +0800

    Update full_backup_data.sh

--- backups/mysqldump/full_backup_data.sh
+++ backups/mysqldump/full_backup_data.sh
@@ -11,6 +11,7 @@ db_host=localhost
 
 [[ ! -d $backup_dir ]] && mkdir -p $backup_dir > /dev/null
 
-mysqldump --user=$db_user --password=$db_passwd --host=$db_host --all-databases --flush-log --master-data=2 --single-transaction -E --trigger -R > $backup_dir/mysql.$(date +%F)
+mysqldump --user=$db_user --password=$db_passwd --host=$db_host --all-databases --flush-log --master-data=2 \
+--single-transaction -E --trigger -R > $backup_dir/mysql.$(date +%F)
 
 find $backup_dir -mtime +5 -exec rm -rf {} \;
